57aa6f97dc6e5eaf142ad706a3407c15cd838841
M.Inomata <inomata@tech-vein.com>
2018-05-06 18:33:26 +0900

    :shirt: コード整理

---
+++ RxSwiftSample/RxSwiftSample/RxCocoaSample/RxCocoaSampleViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  RxCocoaSampleViewModel.swift
+//  RxSwiftSample
+//
+//  Created by Mitsuhiro Inomata on 2018/05/06.
+//  Copyright © 2018年 tech vein, Inc. All rights reserved.
+//
+
+import Foundation
--- RxSwiftSample/RxSwiftSample/UISampleViewController.swift
+++ RxSwiftSample/RxSwiftSample/UISampleViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class UISampleViewController: UIViewController {
+    // UI Outlet
     // リセット
     @IBOutlet private weak var resetButton: UIButton!
 
@@ -40,26 +43,16 @@ class UISampleViewController: UIViewController {
     @IBOutlet private weak var imageContainerView: UIView!
     @IBOutlet private weak var imageView: UIImageView!
 
+
+    // データ
+
+
+    // ---
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
